d4eb3fac1c52b7dd49f52b6a3367fdc0f1dc713a
Aleksandr Kiber <36310292+pricklly@users.noreply.github.com>
2024-02-28 16:47:39 +0300

    Add files via upload

---
+++ unit1.pas
@@ -0,0 +1,63 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Button5: TButton;
+    Button6: TButton;
+    Button7: TButton;
+    Button8: TButton;
+    Button9: TButton;
+    Image1: TImage;
+    rb1: TRadioButton;
+    rb2: TRadioButton;
+    RadioGroup1: TRadioGroup;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if rb1.Checked=True then
+  begin
+    bt1.Caption:="X";
+  end else
+  begin
+    bt1.Caption:="0";
+  end;
+end;
+
+end.
+
